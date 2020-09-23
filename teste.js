e78c92dd8f20e58c2fa13786f83cecef61d07c14
mnerisilva <marceloneri@gmail.com>
2020-09-23 13:42:23 -0300

    version x.xx

--- teste.js
+++ teste.js
@@ -1,8 +1,14 @@
 var iframes = $('iframe')
-var toral_videos = iframes.length
-var iframe1 = iframes[0]
+var total_de_videos = iframes.length
+console.log('Total de vídeos: ' + total_de_videos)
+
+
+for(var i=0;i<=4;i++){
+var iframe1 = iframes[i]
 iframe1.closest('.pagina')
 var div_class_pagina = iframe1.closest('.pagina')
 var item = div_class_pagina.closest('.item')
 var num_pagina = $(item).attr('data-page')
-num_pagina
+console.log('página: ' + num_pagina)    
+}
+
